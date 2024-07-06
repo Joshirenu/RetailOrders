14ed8361072bc1da11855a8edb824fd3031bcdce
Renu Joshi <167344384+Joshirenu@users.noreply.github.com>
2024-07-06 22:46:18 +0530

    RetailOrders.sql

--- SQLQuery_RetailOrders.sql
+++ SQLQuery_RetailOrders.sql
@@ -41,35 +41,6 @@ from (select *, row_number() over(partition by region order by sale desc) as ran
 from cte) as regionwise
 where ranked <= 5;
 
---find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
-
-with cte as(
-select year(order_date) as order_year, month(order_date)as order_month, sum(sale_price) as sales
-from df_orders
-group by year(order_date),month(order_date)
-)
-select order_month, 
-sum(case when order_year = 2022 then sales else 0 end) as sales_2022,
-sum(case when order_year = 2023 then sales else 0 end) as sales_2023
-from cte
-group by order_month
-order by order_month;
-
---for each category which month had highest sales 
-
-with cte as(
-select category, month(order_date)as order_month, sum(sale_price) as sales
-from df_orders
-group by category, month(order_date)
-)
-select * from ( select *,
-row_number() over (partition by category order by sales desc) as ranked
-from cte
-) as ranked_value
-where ranked = 1
-order by sales desc;
-
-
 --which sub category had highest growth by profit in 2023 compare to 2022
 
 with cte as (
@@ -89,3 +60,32 @@ select top 1 *
 ,(sales_2023-sales_2022)
 from  cte2
 order by (sales_2023-sales_2022) desc
+
+--for each category which month had highest sales 
+
+with cte as(
+select category, month(order_date)as order_month, sum(sale_price) as sales
+from df_orders
+group by category, month(order_date)
+)
+select * from ( select *,
+row_number() over (partition by category order by sales desc) as ranked
+from cte
+) as ranked_value
+where ranked = 1
+order by sales desc;
+
+--find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
+
+with cte as(
+select year(order_date) as order_year, month(order_date)as order_month, sum(sale_price) as sales
+from df_orders
+group by year(order_date),month(order_date)
+)
+select order_month, 
+sum(case when order_year = 2022 then sales else 0 end) as sales_2022,
+sum(case when order_year = 2023 then sales else 0 end) as sales_2023
+from cte
+group by order_month
+order by order_month;
+
